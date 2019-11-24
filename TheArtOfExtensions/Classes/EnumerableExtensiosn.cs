3b22cb328b78354e32b577956fba6d30012c10f0
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2019-11-24 19:18:12 +0100

    Add project files.

---
+++ TheArtOfExtensions/Classes/EnumerableExtensiosn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheArtOfExtensions.Classes
+{
+    public static class EnumerableExtensiosn
+    {
+        public static void Upsert<T>(this List<T> target, T upsertee, Predicate<T> areTheSame)
+        {
+            var idx = target.FindIndex(areTheSame);
+            if (idx == -1)
+            {
+                target.Add(upsertee);
+            }
+            else
+            {
+                target[idx] = upsertee;
+            }
+        } 
+    }
+}
---
+++ TheArtOfExtensions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TheArtOfExtensions.Classes;
+
+namespace TheArtOfExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List<T>.Upsert(...)");
+            var vehicles = new List<Vehicle>
+            {
+                new Vehicle {RegNo = "ABC123", Color = "yellow", Year = 2019},
+                new Vehicle {RegNo = "DEF456", Color = "red", Year = 2018}
+            };
+
+            var blue = new Vehicle {RegNo = "GHI789", Color = "blue", Year = 2017};
+            vehicles.Upsert(blue, vehicle => vehicle.RegNo == "GHI789");
+
+            var green = new Vehicle { RegNo = "GHI789", Color = "green", Year = 2017 };
+            vehicles.Upsert(green, vehicle => vehicle.RegNo == "GHI789");
+        }
+    }
+
+    class Vehicle
+    {
+        public string RegNo { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
