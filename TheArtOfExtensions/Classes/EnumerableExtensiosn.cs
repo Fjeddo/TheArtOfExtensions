90645f7a19d87880aadf9c83cb49ae738106c15e
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2019-11-25 07:48:11 +0100

    Added another extension

--- TheArtOfExtensions/Classes/EnumerableExtensiosn.cs
+++ TheArtOfExtensions/Classes/EnumerableExtensiosn.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace TheArtOfExtensions.Classes
 {
@@ -8,15 +7,21 @@ namespace TheArtOfExtensions.Classes
     {
         public static void Upsert<T>(this List<T> target, T upsertee, Predicate<T> equals)
         {
-            var idx = target.FindIndex(equals);
-            if (idx == -1)
-            {
-                target.Add(upsertee);
-            }
-            else
+            if(target.TryFindIndex(equals, out var idx))
             {
                 target[idx] = upsertee;
             }
-        } 
+            else
+            {
+                target.Add(upsertee);
+            }
+        }
+
+        public static bool TryFindIndex<T>(this List<T> target, Predicate<T> equals, out int idx)
+        {
+            idx = target.FindIndex(equals);
+
+            return idx != -1;
+        }
     }
 }
--- TheArtOfExtensions/Program.cs
+++ TheArtOfExtensions/Program.cs
@@ -8,18 +8,33 @@ namespace TheArtOfExtensions
     {
         static void Main()
         {
-            Console.WriteLine("List<T>.Upsert(...)");
             var vehicles = new List<Vehicle>
             {
                 new Vehicle {RegNo = "ABC123", Color = "yellow", Year = 2019},
                 new Vehicle {RegNo = "DEF456", Color = "red", Year = 2018}
             };
 
+            Console.WriteLine("List<T>.TryFindIndex(...)");
+            var didFindIndex = vehicles.TryFindIndex(x => x.RegNo == "DEF456", out var index1);
+            Console.WriteLine($"Vehicle found = {didFindIndex} @ index = {index1}");
+
+            var didNotFindIndex = vehicles.TryFindIndex(x => x.RegNo == "CBA321", out var index2);
+            Console.WriteLine($"Vehicle found = {didNotFindIndex} @ index = {index2}");
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("List<T>.Upsert(...)");
+            
             var blue = new Vehicle {RegNo = "GHI789", Color = "blue", Year = 2017};
             vehicles.Upsert(blue, vehicle => vehicle.RegNo == "GHI789");
+            Console.WriteLine($"List length = {vehicles.Count}");
 
             var green = new Vehicle { RegNo = "GHI789", Color = "green", Year = 2017 };
             vehicles.Upsert(green, vehicle => vehicle.RegNo == "GHI789");
+            Console.WriteLine($"List length = {vehicles.Count}");
+
+            var black = new Vehicle { RegNo = "JKL012", Color = "black", Year = 1977 };
+            vehicles.Upsert(black, vehicle => vehicle.RegNo == "JKL012");
+            Console.WriteLine($"List length = {vehicles.Count}");
         }
     }
 
